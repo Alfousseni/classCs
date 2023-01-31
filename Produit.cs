53e0ed55d9ba8b8a10f45cbee15406242e9b2c00
Alfousseni COULIBALY <coulybalyalfousseni07757@gmail.com>
2023-01-31 14:13:51 +0000

    premier depot

---
+++ Produit.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetProduit
+{
+    internal class Produit
+    {
+        static int incr;
+        public string id;
+        public string code;
+        public string libelle;
+        public string description;
+        public double prix_unitaire;
+        public int quantitestock;
+
+        public Produit(string code, string libelle, string description, double prix_unitaire, int quantitestock)
+        {
+            incr++;
+            this.id = libelle.Substring(0,1) + incr.ToString().PadLeft(3, '0');
+            this.code = code;
+            this.libelle = libelle;
+            this.description = description;
+            this.prix_unitaire = prix_unitaire;
+            this.quantitestock = quantitestock;
+            
+        }
+
+        public  void Affichage()
+        {
+            Console.WriteLine("id :            " + this.id);
+            Console.WriteLine("code :          " + this.code);
+            Console.WriteLine("libelle :       " + this.libelle);
+            Console.WriteLine("Description :   " + this.description);
+            Console.WriteLine("Prix unitaire : " + this.prix_unitaire);
+            Console.WriteLine("Quantiter:      " + this.quantitestock);
+
+            Console.WriteLine("");
+            Console.WriteLine("----------------------");
+        }
+
+        public double Calculprix()
+        {
+            double ptotal=this.prix_unitaire * this.quantitestock;
+
+            return ptotal;
+        }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using projetProduit;
+
+public class program
+{
+    public static void Main(String[] args)
+    {
+       List <Produit> produits= new List <Produit>();
+
+        produits.Add(new Produit("A001", "lait", "produit laitier", 1000,23));
+        produits.Add(new Produit("A002", "sucre", "produit laitier", 1500,18));
+        produits.Add(new Produit("A003", "the", "produit laitier", 1700,49));
+        produits.Add(new Produit("A004", "caffe", "produit laitier", 300,90));
+        produits.Add(new Produit("A005", "kiren", "eau mineral", 500,20));
+        produits.Add(new Produit("A006", "huile", "huile vegetal", 5700, 10));
+        produits.Add(new Produit("A009", "riz", "Aliment", 15000, 100));
+
+
+        foreach (var produit in produits)
+        {
+            produit.Affichage();
+        }
+        Console.WriteLine("----------------Produit donc le Montant en stock depasse 500 000 : --------------------");
+        foreach (var produit in produits)
+        {
+            if (produit.Calculprix() > 500000) { 
+                produit.Affichage();
+            }
+            if (produit.prix_unitaire >= 500)
+            {
+                produit.prix_unitaire -= 100;
+            }
+                                
+        }
+        Console.WriteLine("-----------------apres revision des prix unitaire----------------------------");
+
+        foreach(var produit in produits)
+        {
+            produit.Affichage();
+        }
+
+    }
+}
